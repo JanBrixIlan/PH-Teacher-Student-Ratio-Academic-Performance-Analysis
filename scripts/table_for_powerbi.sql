610ab2502f89e39f7a01cd5ec2d6590bd77859b2
Jan Brix Ilan <164462792+JanBrixIlan@users.noreply.github.com>
2026-06-18 21:12:31 +0800

    Add PowerBI SQL and fix notebook NaN handling
    Add scripts/table_for_powerbi.sql and update notebooks/01_teacher_student_ratio_correlation.ipynb: import sqlalchemy, adjust notebook outputs/execution counts, and handle division-by-zero by replacing infinite values with NaN and filling nulls (deped_master.fillna(0)). Captured DataFrame outputs were also added/updated in the notebook.

---
+++ scripts/table_for_powerbi.sql
@@ -0,0 +1,64 @@
+-- Table to be used for Power BI
+/* Disclaimer: This is the same table in second Jupyter Notebook. Recreated in SQL for better workflow */
+
+
+-- Creating the table for the data from Python
+CREATE TABLE dbo.master_school_data_2023_24 (
+    school_id INT PRIMARY KEY,
+    school_name NVARCHAR(255),
+    region NVARCHAR(100),
+    division NVARCHAR(100),
+
+    kinder_male INT,
+    kinder_female INT,
+
+    g1_male INT,
+    g1_female INT,
+    g2_male INT,
+    g2_female INT,
+    g3_male INT,
+    g3_female INT,
+    g4_male INT,
+    g4_female INT,
+    g5_male INT,
+    g5_female INT,
+    g6_male INT,
+    g6_female INT,
+    g7_male INT,
+    g7_female INT,
+    g8_male INT,
+    g8_female INT,
+    g9_male INT,
+    g9_female INT,
+    g10_male INT,
+    g10_female INT,
+
+    es_master_teacher_iv INT,
+    es_master_teacher_iii INT,
+    es_master_teacher_ii INT,
+    es_master_teacher_i INT,
+
+    es_teacher_iii INT,
+    es_teacher_ii INT,
+    es_teacher_i INT,
+
+    n_test_takers INT,
+
+    filipino_mps FLOAT,
+    english_mps FLOAT,
+    math_mps FLOAT,
+    science_mps FLOAT,
+    araling_panlipunan_mps FLOAT,
+    overall_mps FLOAT
+);
+
+-- Overview of the table
+SELECT * FROM dbo.master_school_data_2023_24
+
+-- Creating a 'country' column for Power BI
+ALTER TABLE dbo.master_school_data_2023_24
+ADD country NVARCHAR(100);
+
+-- Setting 'country' column to 'Philippines'
+UPDATE dbo.master_school_data_2023_24
+SET country = 'Philippines';
